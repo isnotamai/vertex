5a42f4305b9f5577dbcf3306d85e8cfa51d54a1d
Amai <Amai@local>
2026-07-25 01:10:33 +0800

    很強大的更新

--- player.gd
+++ player.gd
@@ -2,21 +2,38 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.002
+
+@onready var head = $Head
+
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+func _input(event):
+	if event is InputEventMouseMotion:
+		#rotate yaw
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		#rotate pitch
+		rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	#gravtity
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+		
+	var input_dir = Input.get_vector("move_left","move_right","move_forward","move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.y + direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
+		
